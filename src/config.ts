cb588dea7311b90b7e337d38d888cc23509b048f
yellowsink <yellowsink@riseup.net>
2023-07-04 18:18:21 +0100

    stuff

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import {sig, effect} from "@uwu/iota"
+
+const nanFallback = (f: number, v: number) => Number.isNaN(v) ? f : v;
+const initFromLs = (key: string, fallback: number) => sig(nanFallback(fallback, parseFloat(localStorage.getItem(key)!)));
+
+export const stretchLength = initFromLs("sl", 30);
+export const fullLength = initFromLs("fl", 5 * 60);
+
+effect(() => localStorage.setItem("sl", stretchLength().toString()));
+effect(() => localStorage.setItem("fl", fullLength().toString()));
